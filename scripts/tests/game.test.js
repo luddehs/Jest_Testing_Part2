5201491b4f8fe4bbaa532c3f5825a03fc2741414
Ludvig Hultgren Svensson <ludvig.hs@gmail.com>
2024-05-15 13:47:12 +0200

    Add lightsOn function with test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+const { afterEach } = require("node:test");
 const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
 beforeAll(() => {
@@ -49,5 +50,28 @@ describe("newGame works correctly", () => {
     });
     test("should add one move to the computer's game array", () => {
         expect(game.currentGame.length).toBe(1);
+    });
+});
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
     });
 });
